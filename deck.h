d92880f33dffc8a0b0eaa4960f4c2d58ef2e1d13
Damilola <samadedammy@gmail.com>
2023-10-24 19:18:27 -0700

    1000-sort_deck.c

--- deck.h
+++ deck.h
@@ -2,13 +2,20 @@
 #define DECK_H
 
 #include <stdlib.h>
-
+/**
+ * enum kind_e - list of static key value
+ * From "SPADE" to "DIAMOND"
+ * @SPADE: spade
+ * @HEART: ht
+ * @CLUB: clu
+ * @DIAMOND: dmd
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -20,8 +27,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -33,9 +40,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
